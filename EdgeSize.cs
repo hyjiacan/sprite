ec2daa9437a3ceeef6bc8a5d7f181fa55016005b
hyjiacan <hyjiacan@163.com>
2019-12-24 15:04:38 +0800

    移除更新功能 减小程序体积

--- EdgeSize.cs
+++ EdgeSize.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CssSprite
+﻿namespace CssSprite
 {
     /// <summary>
     /// 边缘尺寸
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // 有关程序集的常规信息通过下列属性集
@@ -29,5 +28,5 @@ using System.Runtime.InteropServices;
 //      内部版本号
 //      修订号
 //
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("4.3.0.0")]
+[assembly: AssemblyVersion("19.12.24.0")]
+[assembly: AssemblyFileVersion("19.12.24.0")]
